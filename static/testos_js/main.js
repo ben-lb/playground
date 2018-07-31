d80446d719dfc4af2767489e87884ab124c4f445
Ben Batsir <ben@lightbitslabs.com>
2018-07-31 21:33:31 +0300

    subgrids
    Signed-off-by: Ben Batsir <ben@lightbitslabs.com>

--- static/testos_js/main.js
+++ static/testos_js/main.js
@@ -2,35 +2,95 @@ const url = "http://localhost:3000/machines";
 
 setInterval(function() { set_data(); }, 3000); // refresh table evert 3 seconds
 
+const colModel = [
+    { label: 'Name', name: 'name', key: true, width: 130 },
+    { label: 'Type', name: 'type', width: 100 },
+    { label: 'VMs', name: 'vms', hidden: true },
+    { label: 'State', name: 'state', width: 100 },
+    { label: 'Used by', name: 'used_by', width: 100 },
+    { label: 'Labels', name: 'labels', width: 350 },
+    { label: 'Uptime', name: 'uptime', width: 150 },
+    { label: 'Allocation ID', name: 'allocation_id', width: 250 },
+]
+
+
+let colModelVM = [];
+for(let i = 0; i < colModel.length; i++)
+{
+    if(colModel[i]['name'] === 'labels') {
+        continue;
+    }
+    colModelVM.push(colModel[i]);
+}
+
 $(document).ready(function () {
     $("#jqGrid").jqGrid({
         mtype: "GET",
         url: set_data(),
         datatype: 'local',
         styleUI : 'Bootstrap',
-        colModel: [
-            { label: 'Name / ID', name: 'name', key: true, width: 130 },
-            { label: 'Type', name: 'type', width: 100 },
-            { label: 'State', name: 'state', width: 100 },
-            { label: 'Used by', name: 'used_by', width: 100 },
-            { label:'Labels', name: 'labels', width: 350 },
-            { label:'Uptime', name: 'uptime', width: 150 },
-            { label:'Allocation ID', name: 'allocation_id', width: 250 },
-        ],
+        colModel: colModel,
         sortname: "name",
         viewrecords: true,
-        height: 550,
+        height: '100%',
         width: 1500,
         rowNum: 10000,
         cmTemplate: {editable: true},
         sortable: true,
-        pager: "#jqGridPager",
         forceClientSorting: true,
+        subGrid: true,
+        subGridRowExpanded: showChildGrid,
+        subGridOptions : {
+            // expand all rows on load
+            expandOnLoad: true
+        },
+        gridview: false,
+        afterInsertRow: function(rowid, aData, rowelem) {
+            let rowData = $('#jqGrid').getLocalRow(rowid);
+            if(rowData['vms'].length === 0){
+                $('tr#'+rowid, $('#jqGrid')).children("td.sgcollapsed").html("").removeClass('ui-sgcollapsed sgcollapsed');
+            }
+        },
     });
-
-
 });
 
+
+// the event handler on expanding parent row receives two parameters
+// the ID of the grid tow  and the primary key of the row
+function showChildGrid(parentRowID, parentRowKey) {
+    let childGridID = parentRowID + "_table";
+    let childGridPagerID = parentRowID + "_pager";
+
+    // add a table and pager HTML elements to the parent grid row - we will render the child grid here
+    $('#' + parentRowID).append('<table id=' + childGridID + '></table>' +
+        '<div id=' + childGridPagerID + ' class=scroll></div>');
+    let grid_id = "#" + childGridID;
+    $(grid_id).jqGrid({
+        data: get_vms_data_by_parent_row_id(parentRowKey, grid_id),
+        mtype: "GET",
+        datatype: "local",
+        colModel: colModelVM,
+        sortable: true,
+        sortname: 'name',
+        width: 1400,
+        height: '100%',
+    });
+
+}
+
+
+function get_vms_data_by_parent_row_id(row_id, grid_id) {
+    let rowData = $('#jqGrid').getLocalRow(row_id);
+    let rows = [];
+    for(let i = 0; i < rowData['vms'].length; i++)
+    {
+        let vm = rowData['vms'][i];
+        add_machine(rows, vm, false);
+    }
+    return rows;
+    // $(grid_id).jqGrid('clearGridData').jqGrid('setGridParam',
+    //     {datatype: 'local', data: rows}).trigger('reloadGrid');
+}
 
 function set_data() {
     $.ajax({
@@ -39,7 +99,8 @@ function set_data() {
         url: url,
         success: function (data) {
             let organized_data = organize_machines_data(data);
-            $('#jqGrid').jqGrid('clearGridData').jqGrid('setGridParam', {data: organized_data}).trigger('reloadGrid');
+            $('#jqGrid').jqGrid('clearGridData').jqGrid('setGridParam',
+                {datatype: 'local', data: organized_data}).trigger('reloadGrid');
         }
     })
 }
@@ -55,22 +116,28 @@ function organize_machines_data(data) {
             for(let k = 0; k < machine['vms'].length; k++)
             {
                 let vm = machine['vms'][k];
-                add_machine(rows, vm);
+                // add_machine(rows, vm, false);
             }
-            add_machine(rows, machine);
+            add_machine(rows, machine, machine['vms'].length > 0);
         }
 
     }
     return rows;
 }
 
-function add_machine(rows, machine) {
-    rows.push({
-                'name': machine['id'],
-                'type': machine['type'],
-                'state': machine['state'],
-                'used_by': machine['user_name'],
-                'labels': machine['host_label'],
-                'uptime': machine['last_inauguration_date'],
-                'allocation_id': machine['allocation']})
+function add_machine(rows, machine, has_vms) {
+    let data = {
+        'name': machine['id'],
+        'type': machine['type'],
+        'state': machine['state'],
+        'used_by': machine['user_name'],
+        'labels': machine['host_label'],
+        'uptime': machine['last_inauguration_date'],
+        'allocation_id': machine['allocation'],
+        'vms': []};
+    if(has_vms)
+    {
+        data['vms'] = machine['vms']
+    }
+    rows.push(data);
 }
